819ab4210fa50a3e01f12a64432817291809b611
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2022-03-11 20:00:27 +0530

    Create ContainsDuplicates.java

---
+++ Arrays/ContainsDuplicates.java
@@ -0,0 +1,12 @@
+QUESTION: https://leetcode.com/problems/contains-duplicate/
+
+public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> list = new HashSet<>();
+        for(int i = 0;i<nums.length;i++){
+            if(list.contains(nums[i])){
+                return true;
+            }
+            list.add(nums[i]);
+        }
+        return false;
+    }
