8a93b8f556b4a3395485a3a12a92f4f5ed6919b4
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2022-03-13 15:33:36 +0530

    Create ValidParenthesis.java

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,32 @@
+QUESTION: https://leetcode.com/problems/valid-parentheses/
+
+public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '{' || ch == '[' || ch == '('){
+                stack.offerLast(ch);
+            }
+            else if(stack.isEmpty()){
+                return false;
+            }
+            else{
+                if(ch == '}' && stack.peekLast() != '{'){
+                    return false;
+                }
+                else if(ch == ']' && stack.peekLast() != '['){
+                    return false;
+                }
+                else if(ch == ')' && stack.peekLast() != '('){
+                    return false;
+                }
+                else{
+                    stack.pollLast();
+                }
+            }
+        }
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
