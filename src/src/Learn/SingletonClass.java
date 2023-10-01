a1b8ccf4f685c84840950b3726d65ca6fe4d4929
Anirudh Gambhir <anirugam@amazon.com>
2023-10-01 18:24:56 +0530

    Creating double locking Singleton Class

--- src/src/Learn/SingletonClass.java
+++ src/src/Learn/SingletonClass.java
@@ -1,2 +1,20 @@
-package Learn;public class SingletonClass {
+package Learn;
+
+
+public class SingletonClass {
+
+    private static volatile SingletonClass singletonClass;
+
+    private SingletonClass() {
+    }
+
+    public static SingletonClass returnInstance() {
+        if (singletonClass == null) {
+            synchronized (SingletonClass.class) {
+                if (singletonClass == null)
+                    singletonClass = new SingletonClass();
+            }
+        }
+        return singletonClass;
+    }
 }
