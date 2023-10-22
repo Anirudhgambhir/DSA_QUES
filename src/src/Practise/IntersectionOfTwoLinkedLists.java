10c219b999b1cfdcd679be303456795fdb62efa5
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-10-22 14:31:43 +0530

    adding IntersectionOfTwoLinkedLists

---
+++ src/src/Practise/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,24 @@
+package Practise;
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        while (tempA != tempB) {
+            if (tempA == null)
+                tempA = headB;
+            else
+                tempA = tempA.next;
+
+            if (tempB == null)
+                tempB = headA;
+            else
+                tempB = tempB.next;
+        }
+        return tempB;
+    }
+}
