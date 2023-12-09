775af88968cc2bab3b06a681f61e6e260690d5f0
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-12-09 18:41:11 +0530

    adding PowOfXAndN

---
+++ src/src/Practise/PowOfXAndN.java
@@ -0,0 +1,18 @@
+package Practise;
+
+public class PowOfXAndN {
+    public double myPow(double x, int n) {
+        double ans = positivePow(x, Math.abs(n));
+        return n < 0 ? 1 / ans : ans;
+    }
+
+    private double positivePow(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+
+        double ans = myPow(x * x, n / 2);
+        ans = n % 2 != 0 ? ans * x : ans;
+
+        return ans;
+    }
+}
