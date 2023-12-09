c78df7df81efa1446140ed0de5844a66fc2c573c
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-12-09 12:11:46 +0530

    adding merge sort

--- src/src/Learn/Sorting/impl/MergeSort.java
+++ src/src/Learn/Sorting/impl/MergeSort.java
@@ -2,25 +2,47 @@ package Learn.Sorting.impl;
 
 public class MergeSort {
 
-    private int[] merge(int[] a, int n, int[] b, int m) {
-        int[] k = new int[n + m];
+    void merge(int a[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = a[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = a[m + 1 + j];
+
+
         int i = 0;
         int j = 0;
-        int p = 0;
-        while (i < n && j < m) {
-            if (a[i] < b[j])
-                k[p++] = a[i++];
+        int p = l;
+        while (i < n1 && j < n2) {
+            if (L[i] < R[j])
+                a[p++] = L[i++];
             else
-                k[p++] = b[j++];
+                a[p++] = R[j++];
         }
 
-        while (i < n) {
-            k[p++] = a[i++];
+        while (i < n1) {
+            a[p++] = L[i++];
         }
 
-        while (j < m) {
-            k[p++] = b[j++];
+        while (j < n2) {
+            a[p++] = R[j++];
         }
-        return k;
+    }
+    void mergeSort(int a[], int l, int r)
+    {
+        if (r > l) {
+            int mid = l + (r - l) /2;
+            mergeSort(a, l, mid);
+            mergeSort(a, mid + 1, r);
+            merge(a, l, mid, r);
+        }
     }
 }
